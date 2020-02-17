c5609b84151bdfb159187dcc8201b13f45ea1f4b
David Reed <david@ktema.org>
2020-02-16 18:01:56 -0700

    Add Day 4 Part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -21,12 +21,53 @@ fn does_count(pw: i32) -> i32 {
     }
 }
 
+fn does_count_part_2(pw: i32) -> i32 {
+    let str_pw = pw.to_string().into_bytes();
+    let mut has_double = false;
+    let mut last_elem: u8 = 0;
+    let mut elem_count = 0;
+
+    for b in str_pw {
+        if b == last_elem {
+            elem_count += 1;
+        } else {
+            if elem_count == 2 {
+                has_double = true;
+            }
+            elem_count = 1;
+        }
+
+        if b < last_elem {
+            return 0;
+        }
+        last_elem = b
+    }
+
+    if elem_count == 2 {
+        has_double = true;
+    }
+
+    if has_double {
+        1
+    } else {
+        0
+    }
+}
+
 fn main() {
     println!(
         "{}",
         (137683..596253).fold(
             0,
             |acc, x| acc + does_count(x)
+        )
+    );
+
+    println!(
+        "{}",
+        (137683..596253).fold(
+            0,
+            |acc, x| acc + does_count_part_2(x)
         )
     );
 }
