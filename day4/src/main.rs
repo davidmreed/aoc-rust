567ae4ceea826a3008cf256208c411c3d4f3de15
David Reed <david@ktema.org>
2020-02-22 18:09:56 -0700

    Day 4 with no strings

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,73 +1,80 @@
-fn does_count(pw: i32) -> i32 {
-    let str_pw = pw.to_string().into_bytes();
-    let mut has_double = false;
-    let mut last_elem: u8 = 0;
-
-    for b in str_pw {
-        if b == last_elem {
-            has_double = true;
-        }
-
-        if b < last_elem {
-            return 0;
-        }
-        last_elem = b
-    }
-
-    if has_double {
-        1
-    } else {
-        0
-    }
+fn nth_digit(num: u32, digit: u32) -> u32 {
+    // Counting digits UP from the right.
+    // Because we know the length of all of our
+    // numbers, we can get away with this.
+    let plus_one: u32 = 10_u32.pow(digit + 1);
+    let this_one: u32 = 10_u32.pow(digit);
+    ((num % plus_one ) - (num % this_one)) / this_one
 }
 
-fn does_count_part_2(pw: i32) -> i32 {
-    let str_pw = pw.to_string().into_bytes();
-    let mut has_double = false;
-    let mut last_elem: u8 = 0;
-    let mut elem_count = 0;
+fn value_slices(vec: &[u32]) -> Vec<&[u32]> {
+    let mut ret = Vec::new();
+    let mut start_index = 0;
+    let mut val: u32 = vec[0];
 
-    for b in str_pw {
-        if b == last_elem {
-            elem_count += 1;
-        } else {
-            if elem_count == 2 {
-                has_double = true;
-            }
-            elem_count = 1;
+    for index in 1..vec.len() {
+        if vec[index] != val {
+            ret.push(&vec[start_index..index]);
+            val = vec[index];
+            start_index = index;
         }
-
-        if b < last_elem {
-            return 0;
-        }
-        last_elem = b
     }
+    ret.push(&vec[start_index..vec.len()]);
 
-    if elem_count == 2 {
-        has_double = true;
-    }
+    ret
+}
 
-    if has_double {
-        1
-    } else {
-        0
-    }
+fn does_count(pw: u32, f: fn(&&[u32]) -> bool) -> bool {
+    let digits: Vec<u32> = (0..6).map(
+        |digit| nth_digit(pw, 5 - digit)
+    ).collect();
+
+    (1..6).map(
+        |index| digits[index] >= digits[index - 1]
+    ).all(|item| item) 
+    && value_slices(&digits).iter().any(f)
 }
 
 fn main() {
     println!(
         "{}",
-        (137683..596253).fold(
-            0,
-            |acc, x| acc + does_count(x)
-        )
+        (137683..596253).filter(
+            |x| does_count(*x, |&slice| slice.len() >= 2)
+        ).collect::<Vec<u32>>().len()
     );
 
     println!(
         "{}",
-        (137683..596253).fold(
-            0,
-            |acc, x| acc + does_count_part_2(x)
-        )
+        (137683..596253).filter(
+            |x| does_count(*x, |&slice| slice.len() == 2)
+        ).collect::<Vec<u32>>().len()
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nth_digit() {
+        assert_eq!(6, nth_digit(123456, 0));
+        assert_eq!(4, nth_digit(654321, 3));
+        assert_eq!(8, nth_digit(892109, 5));
+    }
+
+    #[test]
+    fn test_value_slices() {
+        let test_data = vec![1, 2, 2, 2, 4, 4, 3];
+
+        assert_eq!(
+            vec![&test_data[0..1], &test_data[1..4], &test_data[4..6], &test_data[6..]],
+            value_slices(&test_data)
+        );
+        let test_data = vec![1, 2, 3, 4];
+
+        assert_eq!(
+            vec![&test_data[0..1], &test_data[1..2], &test_data[2..3], &test_data[3..]],
+            value_slices(&test_data)
+        );
+    }
 }
